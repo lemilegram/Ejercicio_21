b33fc53890144e7e0c266b5cbc30d2ad4e36bc5f
lemilegram <97851079+lemilegram@users.noreply.github.com>
2023-02-09 15:16:36 -0300

    Funciona nodemon de vuelta

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -23,8 +23,8 @@ async function showHome(req, res) {
   res.render("home", { articles });
 }
 
-async function showContact(req, res) {
-  res.render("contact");
+async function showAdmin(req, res) {
+  res.render("admin");
 }
 
 async function showAboutUs(req, res) {
@@ -36,6 +36,6 @@ async function showAboutUs(req, res) {
 
 module.exports = {
   showHome,
-  showContact,
+  showAdmin,
   showAboutUs,
 };
